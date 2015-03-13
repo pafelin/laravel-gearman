cb184992a407b1e8c504a0623c1dc9cfef385873
nikola <nikola@6min.de>
2015-03-13 16:04:55 +0100

    adding/using _setTimeout method

--- src/Pafelin/Gearman/Connectors/GearmanConnector.php
+++ src/Pafelin/Gearman/Connectors/GearmanConnector.php
@@ -15,8 +15,15 @@ class GearmanConnector implements ConnectorInterface {
 
         $client = new GearmanClient;
         $client->addServer($config['host'], (int) $config['port']);
+        $this->_setTimeout($client, $config);
         $worker = new GearmanWorker;
         $worker->addServer($config['host'], (int) $config['port']);
         return new GearmanQueue ($client, $worker, $config['queue']);
+    }
+
+    private function _setTimeout(GearmanClient $client, array $config) {
+        if(isset ($config['timeout']) && is_numeric($config['timeout'])) {
+            $client->setTimeout($config['timeout']);
+        }
     }
 }
